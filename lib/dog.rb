1885e39f190c9a7bb3bb3560568c0a5d49396e88
Kristin <kristinponsonby@gmail.com>
2021-02-15 16:09:38 -0500

    commit tree 6eaa80228fd60aedfe0d632f6cbb6d9b86569afd

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+      end
+    end
+
+  
+
+
+# Checklist
+# {x} Dog method #initialize sets the name of the dog in an instance variable @name
+# {x} Dog method #initialize sets the breed of the dog in an instance variable @breed
+# {} Dog method #initialize defaults the breed argument to "Mutt" in an instance variable @breed
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    def initialize(name)
+        @name = name
+      end
+      def name=(name)
+        @name = name
+      end
+    
+      def name
+        @name
+      end
+    end
+    beyonce = Person.new("Beyonce")
+
+
+
+# Checklist
+# {x} Person method #initialize sets the name of the person in an instance variable @name
